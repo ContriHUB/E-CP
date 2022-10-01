7e100cb7bf4276e8c87a909e9cffd9b0d0c3a264
Achintya Saxena <sachintya05@gmail.com>
2022-10-02 02:11:24 +0530

    Updated CONTRIBUTORS.md and src/Test/Test.py

--- src/Test/Test.py
+++ src/Test/Test.py
@@ -90,10 +90,19 @@ class Test():
             rec_message = client.receive_message()
 
             time_rem = rec_message.message
+            hours = time_rem//3600
+            time_rem = time_rem%3600
             minutes = time_rem//60
             seconds = time_rem%60
 
             print_message = ''
+            if(hours > 0):
+                print_message += f'{hours} hour'
+                if(hours > 1):
+                    print_message += 's'
+
+                print_message += ' '
+            
             if(minutes > 0):
                 print_message += f'{minutes} minute'
                 if(minutes > 1):
