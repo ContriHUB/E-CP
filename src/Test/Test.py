3789868c69da5edf79fb13d6c223b3fc09767169
Achintya Saxena <sachintya05@gmail.com>
2022-10-02 11:42:34 +0530

    Added converter function to return time in hours for time exceeding 59 minutes 59 seconds

--- src/Test/Test.py
+++ src/Test/Test.py
@@ -14,6 +14,31 @@ from .server.ServerThread import ServerThread
 from .Message import Message
 from .client.Client import Client
 
+def print_rem_time(time_rem):
+    print_message = ''
+    hours = time_rem//3600
+    time_rem = time_rem%3600
+    minutes = time_rem//60
+    seconds = time_rem%60
+
+    if(hours > 0):
+        print_message += f'{hours} hour'
+        if(hours > 1):
+            print_message += 's'
+
+        print_message += ' '
+    
+    if(minutes > 0):
+        print_message += f'{minutes} minute'
+        if(minutes > 1):
+            print_message += 's'
+        
+        print_message += ' '
+    
+    print_message += f'{seconds} seconds'
+
+    click.echo(print_message)
+
 class Test():
     def __init__(self) -> None:
         self.HOST = 'localhost'
@@ -88,28 +113,5 @@ class Test():
             client.send_message(send_message)
 
             rec_message = client.receive_message()
-
             time_rem = rec_message.message
-            hours = time_rem//3600
-            time_rem = time_rem%3600
-            minutes = time_rem//60
-            seconds = time_rem%60
-
-            print_message = ''
-            if(hours > 0):
-                print_message += f'{hours} hour'
-                if(hours > 1):
-                    print_message += 's'
-
-                print_message += ' '
-            
-            if(minutes > 0):
-                print_message += f'{minutes} minute'
-                if(minutes > 1):
-                    print_message += 's'
-                
-                print_message += ' '
-            
-            print_message += f'{seconds} seconds'
-
-            click.echo(print_message)
+            print_rem_time(time_rem)
