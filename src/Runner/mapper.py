8f07fc76c8ca6b311024332dc38a2a55514ce020
DNA5769 <dennisthomas2002@gmail.com>
2022-10-02 10:17:45 +0530

    Added mappers for java

--- src/Runner/mapper.py
+++ src/Runner/mapper.py
@@ -2,14 +2,16 @@
     Mappers to map functions and extensions to their respective handlers
 '''
 
-from .runner import run_cpp_code, run_python_code
+from .runner import run_cpp_code, run_java_code, run_python_code
 
 runner_map = {
     'cpp' : run_cpp_code,
-    'python' : run_python_code
+    'python' : run_python_code,
+    'java' : run_java_code
 }
 
 ext_map = {
     'cpp' : 'cpp', 
-    'python' : 'py'
+    'python' : 'py',
+    'java' : 'java',
 }
